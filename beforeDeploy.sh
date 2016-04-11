89cd12870f830249c4baf9ccdebb4ead68d07249
SelmanAY <selmanay@gmail.com>
2016-04-11 15:43:13 +0300

    Travis deploy 4

--- beforeDeploy.sh
+++ beforeDeploy.sh
@@ -1,8 +1,6 @@
-chmod +x ./
+mkdir ./Monitorist/Deploy
+cp ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Monitorist/Deploy/
+cp ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Monitorist/Deploy/Collectors
+cp ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Monitorist/Deploy/Senders
 
-mkdir ./Deploy
-cp ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Deploy/
-cp ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Deploy/Collectors
-cp ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Deploy/Senders
-
-zip -r ./deploy.zip ./Deploy/
+zip -r ./deploy.zip ./Monitorist/Deploy/
