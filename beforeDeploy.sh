227520594b4b443689cea3bc047cbf5f0e4d7c8c
SelmanAY <selmanay@gmail.com>
2016-04-11 17:49:32 +0300

    Travis Deploy 7

--- beforeDeploy.sh
+++ beforeDeploy.sh
@@ -1,6 +1,6 @@
 mkdir ./Monitorist/Deploy
-cp ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Monitorist/Deploy/
-cp ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Monitorist/Deploy/Collectors
-cp ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Monitorist/Deploy/Senders
+cp -avr ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Monitorist/Deploy/
+cp -avr ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Monitorist/Deploy/Collectors
+cp -avr ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Monitorist/Deploy/Senders
 
 zip -r ./deploy.zip ./Monitorist/Deploy/
