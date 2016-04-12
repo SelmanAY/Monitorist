1c1614919a20921380c8d15fdce4e4eb0fb3ece4
SelmanAY <selmanay@gmail.com>
2016-04-12 08:38:19 +0300

    Travis Deploy 10

--- beforeDeploy.sh
+++ beforeDeploy.sh
@@ -1,9 +1,16 @@
 mkdir ./Monitorist/Deploy
+mkdir ./Monitorist/Deploy/Collectors
+mkdir ./Monitorist/Deploy/configs
+mkdir ./Monitorist/Deploy/Senders
+
 cp -avr ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Monitorist/Deploy/
+cp -avr ./Monitorist/Monitorist.Pump.Service/Collectors/*.* ./Monitorist/Deploy/Collectors/
+cp -avr ./Monitorist/Monitorist.Pump.Service/configs/*.* ./Monitorist/Deploy/configs/
+cp -avr ./Monitorist/Monitorist.Pump.Service/Senders/*.* ./Monitorist/Deploy/Senders/
+
 cp -avr ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Monitorist/Deploy/Collectors
 cp -avr ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Monitorist/Deploy/Senders
 
-ls -alR ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* > ./Monitorist/Deploy/releaseFileList.txt
 ls -alR ./Monitorist/Deploy/*.* > ./Monitorist/Deploy/fileList.txt
 
 zip -r ./deploy.zip ./Monitorist/Deploy/*.*
