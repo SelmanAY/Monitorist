731e9a121ba92f6b72582885368921651a598120
Selman AY <selmanay@gmail.com>
2016-04-12 22:03:19 +0300

    Update beforeDeploy.cmd

--- beforeDeploy.cmd
+++ beforeDeploy.cmd
@@ -1,11 +1,14 @@
-md ./Monitorist/Deploy
+md ./Monitorist/Pump
 
-xcopy Monitorist\Monitorist.Pump.Service\bin\Release\*.* Monitorist\Deploy\ /s /e /c /y
-xcopy Monitorist\Monitorist.Pump.Service\Collectors\*.* Monitorist\Deploy\Collectors\ /s /e /c /y
-xcopy Monitorist\Monitorist.Pump.Service\configs\*.* Monitorist\Deploy\configs\ /s /e /c /y
-xcopy Monitorist\Monitorist.Pump.Service\Senders\*.* Monitorist\Deploy\Senders\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\bin\Release\*.* Monitorist\Pump\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\Collectors\*.* Monitorist\Pump\Collectors\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\configs\*.* Monitorist\Pump\configs\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\Senders\*.* Monitorist\Pump\Senders\ /s /e /c /y
 
-xcopy Monitorist\Monitorist.Pump.Collectors\bin\Release\*.dll Monitorist\Deploy\Collectors
-xcopy Monitorist\Monitorist.Pump.GraphiteSender\bin\Release\*.dll Monitorist\Deploy\Senders
+xcopy Monitorist\Monitorist.Pump.Collectors\bin\Release\*.dll Monitorist\Pump\Collectors
+xcopy Monitorist\Monitorist.Pump.GraphiteSender\bin\Release\*.dll Monitorist\Pump\Senders
 
-7z a appVeyorBuild.zip Monitorist\Deploy\*.* -r
+del Monitorist\Pump\*.pdb /a /s
+del Monitorist\Pump\*.xml /a /s
+
+7z a appVeyorBuild.zip Monitorist\Pump\*.* -r
