85fb15f8033c6fdaa9835a78eb062cd96e888b83
Selman AY <selmanay@gmail.com>
2016-04-12 17:16:52 +0300

    Update beforeDeploy.cmd

--- beforeDeploy.cmd
+++ beforeDeploy.cmd
@@ -1,12 +1,9 @@
 md ./Monitorist/Deploy
-md ./Monitorist/Deploy/Collectors
-md ./Monitorist/Deploy/configs
-md ./Monitorist/Deploy/Senders
 
-xcopy Monitorist\Monitorist.Pump.Service\bin\Release\*.* Monitorist\Deploy\ /s /e
-xcopy Monitorist\Monitorist.Pump.Service\Collectors\*.* Monitorist\Deploy\Collectors\ /s /e
-xcopy Monitorist\Monitorist.Pump.Service\configs\*.* Monitorist\Deploy\configs\ /s /e
-xcopy Monitorist\Monitorist.Pump.Service\Senders\*.* Monitorist\Deploy\Senders\ /s /e
+xcopy Monitorist\Monitorist.Pump.Service\bin\Release\*.* Monitorist\Deploy\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\Collectors\*.* Monitorist\Deploy\Collectors\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\configs\*.* Monitorist\Deploy\configs\ /s /e /c /y
+xcopy Monitorist\Monitorist.Pump.Service\Senders\*.* Monitorist\Deploy\Senders\ /s /e /c /y
 
 xcopy Monitorist\Monitorist.Pump.Collectors\bin\Release\*.dll Monitorist\Deploy\Collectors
 xcopy Monitorist\Monitorist.Pump.GraphiteSender\bin\Release\*.dll Monitorist\Deploy\Senders
