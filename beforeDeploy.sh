20808ec70f8f74ecb0fed21112eff4b8f1316de6
SelmanAY <selmanay@gmail.com>
2016-04-12 08:10:24 +0300

    Travis Deploy 8

--- beforeDeploy.sh
+++ beforeDeploy.sh
@@ -3,4 +3,5 @@ cp -avr ./Monitorist/Monitorist.Pump.Service/bin/Release/*.* ./Monitorist/Deploy
 cp -avr ./Monitorist/Monitorist.Pump.Collectors/bin/Release/*.dll ./Monitorist/Deploy/Collectors
 cp -avr ./Monitorist/Monitorist.Pump.GraphiteSender/bin/Release/*.dll ./Monitorist/Deploy/Senders
 
-zip -r ./deploy.zip ./Monitorist/Deploy/
+ls -R ./Monitorist/Deploy/*.* > ./Monitorist/Deploy/fileList.txt
+zip -r ./deploy.zip ./Monitorist/Deploy/*.*
